8b794dda7ac875a2ee1cbb9246a6287824dae503
vijay kumar <308vijaykumar@gmail.com>
2024-11-16 20:34:31 +0530

    login done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
-import Signup from './components/signup'
+import './App.css';
+import Signup from './components/signup';
+import Login from './components/Login';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from './components/MainLayout';
+import Home from './components/Home';
+import Profile from './components/Profile';
+
+// Create the router configuration
+const browserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />, // Main layout for the dashboard pages
+    children: [
+      {
+        path: '/', // Default path for the MainLayout, redirect to /home
+        element: <Home />
+      },
+      {
+        path: 'home', // /home route
+        element: <Home />
+      },
+      {
+        path: 'profile', // /profile route
+        element: <Profile />
+      }
+    ]
+  },
+  {
+    path: '/login', // Login page route
+    element: <Login />
+  },
+  {
+    path: '/signup', // Signup page route
+    element: <Signup />
+  }
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Signup />
+      <RouterProvider router={browserRouter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
